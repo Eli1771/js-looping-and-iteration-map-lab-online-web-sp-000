53fcef6e01729c99e306ece3a0418f2550f9095a
elijahmm17 <elijahmm17@gmail.com>
2019-09-28 00:26:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(drivers) {
+  const lowerCased = [];
+  drivers.map(function(driver) { lowerCased.push(driver.toLowerCase()) });
+  return lowerCased;
+}
+
+function nameToAttributes(drivers) {
+  const madeIntoObj = [];
+  drivers.map(function(driver) { madeIntoObj.push({firstName: driver.split(" ")[0], lastName: driver.split(" ")[1] }) });
+}
